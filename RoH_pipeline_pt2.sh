cec4b753a354289a9763fb3dce7ec224de157271
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2025-09-11 09:25:13 +0200

    Update RoH_pipeline_pt2.sh

--- RoH_pipeline_pt2.sh
+++ RoH_pipeline_pt2.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+
+# Author : Chiara Bortoluzzi
+
+
+#SBATCH -n 1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=10-24:00:00
+#SBATCH --mem-per-cpu=8000
+#SBATCH --job-name=RoH
+#SBATCH --output=output_%J
+#SBATCH --error=error_%J
+
+
+het=$1
+
+
+python runs_of_homozygosity.py --het $het --w 10000 --t1 10 --t2 6000 --t3 0.25 --o RoHs
+
 
